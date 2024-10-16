4873f07b40108895b7ecd3d7222a9073d41da65a
Hyejin Kang <aufkang.hj@gmail.com>
2024-10-16 19:14:10 +0900

    Add detail pages for table and graph

--- recruits/urls.py
+++ recruits/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.job_list, name='job_list'),
+    path('table/', views.job_table, name='job_table'),
+    path('graph/', views.job_graph, name='job_graph'),
 ]
